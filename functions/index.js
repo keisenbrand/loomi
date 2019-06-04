4e7daa8d24322ac9e609a179fe4c8617951f95bd
Katherine Eisenbrand <keisenbrand@gmail.com>
2019-06-03 18:52:08 -0700

    api for getPartner and getHomeStatus

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,55 @@
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+// Get home status given a userID
+exports.getHomeStatus = functions.https.onRequest((req, res) => {
+  // const params = req.url.split("/");
+  // console.log(params);
+  // const userId = params[1];
+  const userID = 'user1';
+  var ref = admin.database().ref('users/' + userID);
+  var isHome;
+  ref.once('value', (snapshot) => {
+    var isHome = snapshot.child('isHome').val();
+    console.log(isHome);
+  }).catch(error => {
+    console.error(error);
+    res.error(500);
+  });
+  res.send(isHome);
+});
+
+// Get user's partner given their userID
+exports.getPartner = functions.https.onRequest((req, res) => {
+  // const params = req.url.split("/");
+  // console.log(params);
+  // const userId = params[1];
+  const userID = 'user1';
+  var ref = admin.database().ref('users/' + userID);
+  var partner;
+  ref.once('value', (snapshot) => {
+    partner = snapshot.child('partner').val();
+  }).catch(error => {
+    console.error(error);
+    res.error(500);
+  });
+  res.send(partner);
+});
+
+// Update user's home status when they get home or leave home (handled on client side)
